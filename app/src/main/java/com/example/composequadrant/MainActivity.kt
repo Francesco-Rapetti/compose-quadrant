947864ecf7ef635cf976fb6e0ef9ac488328fa16
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-26 21:13:33 +0100

    Fix quadrant weight problem

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -57,6 +55,8 @@ fun Quadrant(title: String, description: String, color: Color, modifier: Modifie
         Column(
             modifier = modifier
                 .padding(16.dp)
+                .align(alignment = Alignment.Center),
+
         ) {
             Text(
                 text = title,
@@ -88,7 +88,8 @@ fun Page() {
             Quadrant(
                 title = "Text composable",
                 description = "Displays text and follows the recommended Material Design guidelines.",
-                color = Color(0xFFEADDFF)
+                color = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
             )
             Quadrant(
                 title = "Image composable",
