7f3ae3bc4a9e9fae60ed91039c1987d6aaf0af6b
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-26 22:11:49 +0100

    Center quadrant's text

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -22,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,15 +54,17 @@ fun Quadrant(title: String, description: String, color: Color, modifier: Modifie
             .fillMaxHeight(1f)
     ) {
         Column(
+            verticalArrangement = Arrangement.Center,
             modifier = modifier
                 .padding(16.dp)
-
+                .fillMaxHeight(1f)
 
 
         ) {
             Text(
                 text = title,
                 fontWeight = FontWeight.Bold,
+                color = Color.Black,
                 modifier = Modifier
                     .padding(bottom = 16.dp)
                     .align(alignment = Alignment.CenterHorizontally)
@@ -68,7 +72,9 @@ fun Quadrant(title: String, description: String, color: Color, modifier: Modifie
 
             Text(
                 text = description,
-                textAlign = TextAlign.Justify
+                color = Color.Black,
+                textAlign = TextAlign.Justify,
+                lineHeight = 24.sp,
             )
         }
     }
